6fccaf4e6ec9c21bdec0860f3885c9e0d20ca3d3
Ivy <ivystacyn@gmail.com>
2023-03-07 08:40:05 -0800

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end=""
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
