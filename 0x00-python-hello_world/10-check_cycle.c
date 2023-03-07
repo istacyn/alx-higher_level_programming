3eb0245ff53e007223425be54c611d614ec2aace
Ivy <ivystacyn@gmail.com>
2023-03-07 02:07:06 -0800

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,17 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow, *fast;
 
 	if (!list)
 		return (0);
 
-	while (slow && fast && fast->next)
+	slow = fast = list;
+	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
+
 		if (slow == fast)
 			return (1);
 	}
