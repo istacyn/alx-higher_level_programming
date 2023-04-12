4f6737ef09ee0a5db3782172b9a73b60fcabc55a
Ivy <ivystacyn@gmail.com>
2023-04-12 11:41:28 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,11 @@
 
 
 import sys
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
 
+if __name-- == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 # Load current list from file or create empty list
 try:
