7437a4b8031676179cdab7f41ef6c7dd4457e304
Ivy <ivystacyn@gmail.com>
2023-04-12 06:46:23 -0700

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     my_obj: The object to write to the file
     filename: Name of file to write object to
     """
-    with open('filename', 'w') as file:
+    with open(filename, 'w') as file:
         json.dump(my_obj, file)
