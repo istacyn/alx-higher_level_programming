3959e347ee561c9e1073b87c78a52d033cd87792
Ivy <ivystacyn@gmail.com>
2023-04-12 11:48:36 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,14 +10,9 @@ if __name-- == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
 # Load current list from file or create empty list
-try:
-    list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    list = []
-
-# Add command-line arguments to the list
-for arg in sys.argv[1:]:
-    list.append(arg)
-
-# Save updated list to file
-save_to_json_file(list, "add_item.json")
+    try:
+        list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        list = []
+    list.extend(sys.argv[1:])
+    save_to_json_file(list, "add_item.json")
