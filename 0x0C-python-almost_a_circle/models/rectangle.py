26399a67d9d3542cd25afad43e5470e311d74676
Ivy <ivystacyn@gmail.com>
2023-04-18 00:40:22 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,8 +118,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """Create a string representation of Rectangle class"""
-        return f"[Rectangle]({self.id}) {self.__x}/{self.__y} -
-    {self.__width}/{self.__height}"
+        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
+                                                self.id, self.__x, self.__y,
+                                                self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """Updates attributes of Rectangle class
