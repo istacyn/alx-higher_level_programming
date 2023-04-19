40024feadd38bfcc3f9fbcaabe595b75df85e2f0
Ivy <ivystacyn@gmail.com>
2023-04-19 08:50:32 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,6 +41,9 @@ class Base:
         of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
+        if (type(list_dictionaries) != list or not
+                all(type(i) == dict for i in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
     @classmethod
