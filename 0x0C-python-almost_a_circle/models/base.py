93340697d3d955982e0a502068bbed59a390332f
Ivy <ivystacyn@gmail.com>
2023-04-19 01:31:01 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ all other classes in this project"""
 
 
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -66,20 +68,120 @@ class Base:
         """Returns an instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy = cls(1)
         else:
-            dummy = cls()
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
     @classmethod
     def load_from_file(cls):
         """Returns a list of instances"""
-        filename = str(cls.__name__) + ".json"
+        filename = f"{cls.__name__}.json"
         try:
-            with open(filename, "r") as jsonfile:
-                list_dicts = Base.from_json_string(jsonfile.read())
-                return [cls.create(d) for d in list_dicts]
-        except IOError:
+            with open(filename, "r") as f:
+                data = json.load(f)
+        except IOError as e:
+            print(f"Error loading from file {filename}: {e}")
             return []
+
+        return [cls.create(**d) for d in data]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Saves a list of objects to a CSV file.
+
+        Args:
+        list_objs (list): A list of objects to save.
+
+        Returns:
+        None
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as f:
+            writer = csv.writer(f)
+            if cls.__name__ == "Rectangle":
+                fields = ["id", "width", "height", "x", "y"]
+            else:
+                fields = ["id", "size", "x", "y"]
+            writer.writerow(fields)
+            for obj in list_objs:
+                row = [getattr(obj, field) for field in fields]
+                writer.writerow(row)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Load a list of objects from a CSV file.
+
+        Returns:
+        A list of objects loaded from the file.
+        An empty list if the file does not exist or is empty
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as f:
+                reader = csv.reader(f)
+                if cls.__name__ == "Rectangle":
+                    fields = ["id", "width", "height", "x", "y"]
+                else:
+                    fields = ["id", "size", "x", "y"]
+                next(reader)
+                list_dicts = [{field: int(row[i]) for i,
+                               field in enumerate(fields)} for row in reader]
+                list_objs = [cls.create(**d) for d in list_dicts]
+                return list_objs
+        except FileNotFoundError:
+            return []
+        except csv.Error:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """
+        Opens a window and draws all the Rectangles and Squares.
+
+        Args:
+        list_rectangles (list): A list of Rectangle objects to draw.
+        list_squares (list): A list of Square objects to draw.
+        """
+        window = turtle.Screen()
+        window.bgcolor("white")
+
+        # cretae a turtle to draw the shapes
+        t = turtle.Turtle
+        t.pensize(5)
+        t.shape("turtle")
+        t.color("red")
+
+        # draw rectangle shape
+        for r in list_rectangles:
+            t.penup()
+            t.goto(r.x, r.y)
+            t.pendown()
+            t.setheading(0)
+            t.forward(r.width)
+            t.left(90)
+            t.forward(r.height)
+            t.left(90)
+            t.forward(r.width)
+            t.left(90)
+            t.forward(r.height)
+            t.left(90)
+
+        # draw square shape
+        for s in list_squares:
+            t.penup()
+            t.goto(s.x, s.y)
+            t.pendown()
+            t.setheading(0)
+            t.forward(r.size)
+            t.left(90)
+            t.forward(r.size)
+            t.left(90)
+            t.forward(r.size)
+            t.left(90)
+            t.forward(r.size)
+            t.left(90)
+
+        turtle.exitonclick()
