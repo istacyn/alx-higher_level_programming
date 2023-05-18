db95e055a82dce524650a7af4e12897c1f9eea32
Ivy <ivystacyn@gmail.com>
2023-05-18 05:05:36 -0700

    Task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
 FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
 GROUP BY tv_shows.id
 ORDER BY rating_sum DESC;
