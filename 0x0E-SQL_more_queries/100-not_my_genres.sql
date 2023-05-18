0d98752059243c9f6b92e3426b9639b7d990bfc7
Ivy <ivystacyn@gmail.com>
2023-05-18 02:42:36 -0700

    Task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,7 +5,8 @@ FROM tv_genres
 WHERE tv_genres.id NOT IN (
 	SELECT tv_show_genres.genre_id
 	FROM tv_show_genres
-	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title - 'Dexter'
+	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
