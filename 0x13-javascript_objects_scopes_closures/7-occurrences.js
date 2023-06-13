f4876979d24195fdf9a483aeed079148eb79b339
Ivy <ivystacyn@gmail.com>
2023-06-13 12:31:17 +0000

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,12 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (i = 0; i < list.length; i++) {
+
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-	  count++;
+      count++;
     }
   }
+
   return count;
-}
+};
