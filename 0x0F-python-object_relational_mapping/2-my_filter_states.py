b294496485ca6a182fae4fcb80db1fe114393e9f
Ivy <ivystacyn@gmail.com>
2023-06-16 12:14:39 +0000

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states from the database starting with 'N'"""
+
+import MySQLdb as mysql
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    """
+    Access the database and retrieve the list of states starting with N.
+    """
+    db = mysql.connect(host="localhost", port=3306,
+                       user=argv[1], passwd=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = %s \
+                   ORDER BY states.id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
