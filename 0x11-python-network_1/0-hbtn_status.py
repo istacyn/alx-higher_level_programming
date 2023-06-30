6675c7d02b388fb91b07e0cafd40fefd0ccdabd0
Ivy <ivystacyn@gmail.com>
2023-06-30 03:16:40 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,12 @@ Fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
+
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-
-    with urllib.request.urlopen(url) as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
-        utf8_content = body.decode('utf-8')
-
-    print("- Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", utf8_content)
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
